cfb9ae177ed38f2716ca99ff3254bf9c53fd922e
Sam <30577766+Samasaur1@users.noreply.github.com>
2025-05-14 02:48:16 -0700

    set matching "legacy highlights"
    this was done out of laziness but maybe is actually the right way to do
    it? because the legaciness of these highlights is not tied to a specific
    language

--- default.nix
+++ default.nix
@@ -33,12 +33,82 @@ let
     ) everything
   );
 
+  theme = builtins.fromJSON (builtins.readFile ./catppuccin-mocha.json);
+  theme' = theme // {
+    "parameter" = theme."variable.parameter" or null;
+    "field" = theme."variable.member" or null;
+    "namespace" = theme."module" or null;
+    "float" = theme."number.float" or null;
+    "symbol" = theme."string.special.symbol" or null;
+    "string.regex" = theme."string.regexp" or null;
+
+    "text" = theme."markup" or null;
+    "text.strong" = theme."markup.strong" or null;
+    "text.emphasis" = theme."markup.italic" or null;
+    "text.underline" = theme."markup.underline" or null;
+    "text.strike" = theme."markup.strikethrough" or null;
+    "text.uri" = theme."markup.link.url" or null;
+    "text.math" = theme."markup.math" or null;
+    "text.environment" = theme."markup.environment" or null;
+    "text.environment.name" = theme."markup.environment.name" or null;
+
+    "text.title" = theme."markup.heading" or null;
+    "text.literal" = theme."markup.raw" or null;
+    "text.reference" = theme."markup.link" or null;
+
+    "text.todo.checked" = theme."markup.list.checked" or null;
+    "text.todo.unchecked" = theme."markup.list.unchecked" or null;
+
+    "comment.note" = theme."comment.hint" or null;
+
+    # @text.todo is now for todo comments, not todo notes like in markdown
+    "text.todo" = theme."comment.todo" or null;
+    "text.warning" = theme."comment.warning" or null;
+    "text.note" = theme."comment.note" or null;
+    "text.danger" = theme."comment.error" or null;
+
+    # # @text.uri is now
+    # # > @markup.link.url in markup links
+    # # > @string.special.url outside of markup
+    # "text.uri" = theme."markup.link.uri" or null;
+
+    "method" = theme."function.method" or null;
+    "method.call" = theme."function.method.call" or null;
+
+    "text.diff.add" = theme."diff.plus" or null;
+    "text.diff.delete" = theme."diff.minus" or null;
+
+    "type.qualifier" = theme."keyword.modifier" or null;
+    "keyword.storage" = theme."keyword.modifier" or null;
+    "define" = theme."keyword.directive.define" or null;
+    "preproc" = theme."keyword.directive" or null;
+    "storageclass" = theme."keyword.storage" or null;
+    "conditional" = theme."keyword.conditional" or null;
+    "exception" = theme."keyword.exception" or null;
+    "include" = theme."keyword.import" or null;
+    "repeat" = theme."keyword.repeat" or null;
+
+    "symbol.ruby" = theme."string.special.symbol.ruby" or null;
+
+    "variable.member.yaml" = theme."field.yaml" or null;
+
+    "text.title.1.markdown" = theme."markup.heading.1.markdown" or null;
+    "text.title.2.markdown" = theme."markup.heading.2.markdown" or null;
+    "text.title.3.markdown" = theme."markup.heading.3.markdown" or null;
+    "text.title.4.markdown" = theme."markup.heading.4.markdown" or null;
+    "text.title.5.markdown" = theme."markup.heading.5.markdown" or null;
+    "text.title.6.markdown" = theme."markup.heading.6.markdown" or null;
+
+    "method.php" = theme."function.method.php" or null;
+    "method.call.php" = theme."function.method.call.php" or null;
+  };
+
   treesitterConfig = {
     parser-directories = [
       grammarDirectories
     ];
     # theme stuff
-    theme = builtins.fromJSON (builtins.readFile ./catppuccin-mocha.json);
+    theme = theme';
   };
 
   treesitterConfigPath = pkgs.writeText "config.json" (builtins.toJSON treesitterConfig);
